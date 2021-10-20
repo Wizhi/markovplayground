758e98aecf5b19106822eecd105c873dc63b6adf
Simon Mikkelsen <simonbodall@runbox.com>
2021-10-21 00:07:01 +0200

    Use a spectrum approach to transitions
    Now the next state is actually chosen based on it's probability of
    occuring.
    Unsure if this is the most optimal in terms of memory usage, but that's
    an exercise for later.

--- main.go
+++ main.go
@@ -8,24 +8,38 @@ import (
 
 func main() {
 	options := [...]string{"Bear", "Bull", "Stagnant"}
-	transition := [...][3]float32{
+	transition := [...][3]float64{
 		{0.9, 0.075, 0.025},
 		{0.15, 0.8, 0.05},
 		{0.25, 0.25, 0.5},
+	}
+
+	spectrum := make([][]float64, len(transition))
+
+	for i, p := range transition {
+		a := make([]float64, len(p))
+
+		for j, acc := 0, float64(0); j < len(a); j++ {
+			acc += p[j]
+			a[j] = acc
+		}
+
+		spectrum[i] = a
 	}
 
 	rng := rand.New(rand.NewSource(time.Now().Unix()))
 
 	state := 0
 
-	for {
+	for i := 0; i < 10; i++ {
 		fmt.Println(options[state])
 
-		p := transition[state]
+		r := rng.Float64()
+		s := spectrum[state]
 
-		for i := range p {
-			if p[i] < rng.Float32() {
-				state = i
+		for j := range s {
+			if r < s[j] {
+				state = j
 				break
 			}
 		}
