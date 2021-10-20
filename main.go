e3e3b929b57903b5d1c34b9fc9a022d7ce6e7b2c
Simon Mikkelsen <simonbodall@runbox.com>
2021-10-20 23:46:35 +0200

    Simplify code

--- main.go
+++ main.go
@@ -7,30 +7,19 @@ import (
 )
 
 func main() {
-	const (
-		Bear = iota
-		Bull
-		Stagnant
-	)
-
-	labels := map[int]string{
-		Bear:     "Bear",
-		Bull:     "Bull",
-		Stagnant: "Stagnant",
+	options := [...]string{"Bear", "Bull", "Stagnant"}
+	transition := [...][3]float32{
+		{0.9, 0.075, 0.025},
+		{0.15, 0.8, 0.05},
+		{0.25, 0.25, 0.5},
 	}
-
-	var transition [3][3]float32
-
-	transition[Bear] = [3]float32{0.9, 0.075, 0.025}
-	transition[Bull] = [3]float32{0.15, 0.8, 0.05}
-	transition[Stagnant] = [3]float32{0.25, 0.25, 0.5}
 
 	rng := rand.New(rand.NewSource(time.Now().Unix()))
 
-	state := Bear
+	state := 0
 
 	for {
-		fmt.Println(labels[state])
+		fmt.Println(options[state])
 
 		p := transition[state]
 
